efe2105f0546c1d9d3341272e15e982530dfbb77
vadamk <volodymyr.kravch@gmail.com>
2019-06-27 20:26:00 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import converter from 'json-2-csv';
+import downloadFile from 'js-file-download';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.jsonRef = React.createRef();
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    const { value } = this.jsonRef.current;
+    const obj = JSON.parse(value);
+    const data = obj.Data[0].UserReportDayModels.map(item => {
+
+      const projects = {}
+
+      item.UserProjectTimeLogViewModels.forEach((pr) => {
+        projects[pr.Name] = {
+          Hours: pr.OfficialHoursModel.Hours || 0,
+          Description: pr.OfficialHoursModel.Description || ''
+        }
+      });
+
+      return {
+        date: new Date(item.Day),
+        ...projects
+      }
+    })
+
+    console.log('data: ', data);
+    converter.json2csv(data, (err, res) => {
+      if (err) { throw err }
+      downloadFile(res, 'report.csv')
+    });
+  }
+
+  render() {
+    return <div>
+      <textarea
+        ref={this.jsonRef}
+        name="json"
+        id="json"
+        cols="200"
+        rows="40">
+      </textarea>
+      <button onClick={this.handleClick}>Convert</button>
     </div>
-  );
+  }
 }
 
 export default App;
